313b8f19f3870ca5ab05a888852bbd5de2914799
FoxyJr <foxyjr@live.fr>
2017-01-22 15:39:49 +0100

    Rails 5.0.1

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20161125155243) do
   create_table "jeus", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
-    t.string   "rank"
+    t.integer  "rank"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "console_id"
